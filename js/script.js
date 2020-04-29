af4830fbf41de17d94884218f02e9eecc8910c73
Zero Blitz <raffibaihaqy02@gmail.com>
2020-04-29 22:02:33 +0700

    some changes

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 function allMenu(){
     $.getJSON('data/sarapan-pagi.json', function(data){
         let menu = data.menu;
+        let content = '';
         menu.sort(function(a, b){
-            return sortAlphabet(a.nama, b.nama);
-            return a.harga - b.harga;
+            return sortAlphabet(a.nama, b.nama) && a.harga - b.harga;
         });
         $.each(menu, function(i, data){
             $('#daftar-menu').append('<div class="col-md-3"><div class="card mb-3"><img src="image/sarapan_pagi/'+ data.gambar +'" style="background-color: #FFFED9" class="card-img-top"><div class="card-body"><h5 class="card-title">'+ data.nama +'</h5><p class="card-text">'+ data.deskripsi +'</p><h5 class="card-title" id="termurah">Rp. '+ data.harga +'</h5><a href="#" class="btn btn-success">Pesan Sekarang</a></div></div></div>');
@@ -30,8 +30,7 @@ $('.nav-link').on('click', function(){
         let menu = data.menu;
         let content = '';
         menu.sort(function(a, b){
-            return sortAlphabet(a.nama, b.nama);
-            return a.harga - b.harga;
+            return sortAlphabet(a.nama, b.nama) && a.harga - b.harga;
         });
 
         $.each(menu, function(i, data){
